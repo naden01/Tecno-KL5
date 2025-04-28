5ad9ff4e418e6829d20b37e2106f2f72db90e58d
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-04-28 09:34:11 -0700

    try decryption

--- device.mk
+++ device.mk
@@ -64,7 +64,26 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     mtk_plpath_utils \
     mtk_plpath_utils.recovery
-	
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1
+
+# Additional Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hardware.keymaster@4.1 \
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1
+
+# Keystore2
+PRODUCT_PACKAGES += \
+    android.system.keystore2
+
+# Drm
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
